7ef331341ea465eb4997f9c14bc4c96082058cd4
barrard <barrard@gmail.com>
2019-06-19 14:26:58 -1000

    fixing links

--- index.php
+++ index.php
@@ -9,12 +9,14 @@
 <div class='hidden'>
   <div id='getStatsFromDB'></div>
   <div id='resumeGame'>resumeGame</div>
-<?php echo 'teamName =  <span id="teamName">'.$_SESSION['teamName'].'</span><br>'; ?>
-<?php echo 'teamId =  <span id="teamId">'.$_SESSION['teamId'].'</span><br>'; ?>
-<?php echo 'opponentName = <span id="opponentName">'.$_SESSION['opponentName'].'</span><br>'; ?>
-<?php echo 'opponentId =  <span id="opponentId">'.$_SESSION['opponentId'].'</span><br>'; ?>
-<?php echo 'gameId =  <span id="gameId">'.$_SESSION['gameId'].'</span><br>'; ?>
-<?php print_r($_SESSION); ?>
+<?php 
+//  echo 'teamName =  <span id="teamName">'.$_SESSION['teamName'].'</span><br>'; 
+//  echo 'teamId =  <span id="teamId">'.$_SESSION['teamId'].'</span><br>'; 
+//  echo 'opponentName = <span id="opponentName">'.$_SESSION['opponentName'].'</span><br>'; 
+//  echo 'opponentId =  <span id="opponentId">'.$_SESSION['opponentId'].'</span><br>'; 
+//  echo 'gameId =  <span id="gameId">'.$_SESSION['gameId'].'</span><br>'; 
+//  print_r($_SESSION); 
+ ?>
 </div>
 
 
